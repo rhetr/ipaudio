23e30ac7841c449a5f989f8ddcbc2c1924eeca7f
rhetr <rhetr@users.noreply.github.com>
2018-02-20 16:10:03 -0500

    autorun alsamixer on tty1

--- setup.sh
+++ setup.sh
@@ -4,13 +4,14 @@
 ROLE='source'
 USERNAME=perry
 
-SRC_DIR=/home/$USERNAME/deploy/ipaudio
+USER_HOME=/home/$USERNAME
+SRC_DIR=/$USER_HOME/deploy/ipaudio
 SYSTEMD_SRC_DIR=$SRC_DIR/systemd
 SYSTEMD_USER_DIR=/home/$USERNAME/.config/systemd/user
 SYSTEMD_SET_UNIT="ipaudio-$ROLE"
 
 ## install required stuff
-apt install -y jackd zita-njbridge
+apt install -y jackd zita-njbridge alsa-utils
 
 ## copy stuff to SYSTEMD_USR_DIR
 cp -pr $SYSTEMD_SRC_DIR $SYSTEMD_USER_DIR
@@ -18,3 +19,6 @@ cp -pr $SYSTEMD_SRC_DIR $SYSTEMD_USER_DIR
 # set role
 systemctl --user enable $SYSTEMD_SET_UNIT
 loginctl enable-linger $USERNAME
+
+# pull up alsamixer for tty1, should be automatic
+echo '[[ $(tty) = "/dev/tty1" ]] && exec alsamixer' >> $USER_HOME/.bashrc
