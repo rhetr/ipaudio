5314cfa4d73f048b95c3c8b08d0d92b393938b51
rhetr <rhetr@users.noreply.github.com>
2018-02-26 15:17:38 -0500

    fixed connect-zita-n2j.service and added systemd-user to pam.d

--- setup.sh
+++ setup.sh
@@ -5,19 +5,29 @@ ROLE='source'
 USERNAME=perry
 
 USER_HOME=/home/$USERNAME
-SRC_DIR=/$USER_HOME/deploy/ipaudio
+SRC_DIR=$USER_HOME/deploy/ipaudio
 SYSTEMD_SRC_DIR=$SRC_DIR/systemd
-SYSTEMD_USER_DIR=/home/$USERNAME/.config/systemd/user
+SYSTEMD_USER_DIR=$USER_HOME/.config/systemd/user
 SYSTEMD_SET_UNIT="ipaudio-$ROLE"
 
 ## install required stuff
 apt install -y jackd zita-njbridge alsa-utils
 
 ## copy stuff to SYSTEMD_USR_DIR
-cp -pr $SYSTEMD_SRC_DIR $SYSTEMD_USER_DIR
+su -c "mkdir -p $SYSTEMD_USER_DIR" $USERNAME
+
+cp -p $SYSTEMD_SRC_DIR/jackd.service $SYSTEMD_USER_DIR
+cp -p $SYSTEMD_SRC_DIR/ipaudio-sink/* $SYSTEMD_USER_DIR
+cp -p $SYSTEMD_SRC_DIR/ipaudio-source/* $SYSTEMD_USER_DIR
+
+cp $SRC_DIR/wait_zita /usr/local/bin/
+
+# add pam_limits.so to /etc/pam.d/systemd-user
+echo "session  required pam_limits.so" >> /etc/pam.d/systemd-user
 
 # set role
-systemctl --user enable $SYSTEMD_SET_UNIT
+su -c "systemctl --user daemon-reload" $USERNAME
+su -c "systemctl --user enable $SYSTEMD_SET_UNIT" $USERNAME
 loginctl enable-linger $USERNAME
 
 # pull up alsamixer for tty1, should be automatic
